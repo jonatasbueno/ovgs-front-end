2ce12c058dd9e513641472c56e9aa741f30eed4a
Jonatas Livramento <jonatas.livramento@gmail.com>
2026-07-06 20:26:42 -0300

    chore: update dependencies, add Playwright configuration, and enhance testing setup

---
+++ src/test/setup.ts
@@ -0,0 +1,28 @@
+import "@testing-library/jest-dom/vitest";
+
+// Polyfills para componentes Radix UI (Select, Dialog) no jsdom.
+class ResizeObserverStub {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+if (!("ResizeObserver" in globalThis)) {
+  Object.defineProperty(globalThis, "ResizeObserver", {
+    value: ResizeObserverStub,
+    writable: true,
+  });
+}
+
+if (!Element.prototype.scrollIntoView) {
+  Element.prototype.scrollIntoView = () => {};
+}
+if (!Element.prototype.hasPointerCapture) {
+  Element.prototype.hasPointerCapture = () => false;
+}
+if (!Element.prototype.setPointerCapture) {
+  Element.prototype.setPointerCapture = () => {};
+}
+if (!Element.prototype.releasePointerCapture) {
+  Element.prototype.releasePointerCapture = () => {};
+}
